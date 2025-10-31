d501e12de0cc45f2bb4e2a3c45d56bf18b440970
Kyle Burt <arrant-pettier2u@icloud.com>
2025-10-31 02:21:56 -0700

    Add playRound function
    Plays through one round and logs the winner.

--- javascript.js
+++ javascript.js
@@ -21,3 +21,43 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your selection");
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice) {
+        console.log(`${humanChoice} vs ${computerChoice}... Its a tie!`);
+    }
+    else if (humanChoice == "rock") {
+        if (computerChoice != "paper") {
+            humanScore++;
+            console.log(`You Win! Rock beats ${computerChoice}`)
+        }
+        else {
+            computerScore++;
+            console.log(`You Lose! ${computerChoice} beats Rock`)
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice != "scissors") {
+            humanScore++;
+            console.log(`You Win! Paper beats ${computerChoice}`)
+        }
+        else {
+            computerScore++;
+            console.log(`You Lose! ${computerChoice} beats paper`)
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice != "rock") {
+            humanScore++;
+            console.log(`You Win! Scissors beats ${computerChoice}`)
+        }
+        else {
+            computerScore++;
+            console.log(`You Lose! ${computerChoice} beats scissors`)
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
