57ef4250180a9622a74e0ef663b9f470e7fba5f6
Kyle Burt <arrant-pettier2u@icloud.com>
2025-10-31 01:41:27 -0700

    Add getComputerChoice function
    Randomly selects an integer between 0-2 and assigns that number to the
    choice of 'rock', 'paper', or 'scissors' and returns the result.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    let choiceNumber = Math.floor(Math.random() * 3);
+    switch(choiceNumber) {
+        case 0:
+            return "rock";
+        
+        case 1:
+            return "paper";
+
+        case 2:
+            return "scissors";
+    }
+}
