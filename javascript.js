94f5a5c1fcc72f8fbcd617245b078d85e948ca0f
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-03 17:01:19 -0800

    Add winner display once a player has won

--- javascript.js
+++ javascript.js
@@ -1,23 +1,6 @@
 console.log("Hello World");
 
-let humanScore = 0;
-let computerScore = 0;
-
-const menu = document.querySelector("#menu");
-const results = document.querySelector("#results");
-
-const roundOutcome = document.createElement("h3");
-const humanScoreText = document.createElement("p");
-const computerScoreText = document.createElement("p");
-
-menu.addEventListener("click", (event) => {
-    let target = event.target;
-
-    const computerSelection = getComputerChoice();
-    playRound(target.id, computerSelection);
-});
-
-
+playGame();
 
 function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random() * 3);
@@ -33,44 +16,52 @@ function getComputerChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
-        roundOutcome.textContent = `${humanChoice} vs ${computerChoice}... Its a tie!`;
-    } else if (
-        (humanChoice == "rock" && computerChoice != "paper") ||
-        (humanChoice == "paper" && computerChoice != "scissors") ||
-        (humanChoice == "scissors" && computerChoice != "rock") ) {
-        
-        humanScore++;
-        roundOutcome.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
-    } else {
-        computerScore++;
-        roundOutcome.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
-    }
-
-    humanScoreText.textContent = `Player Score: ${humanScore}`;
-    computerScoreText.textContent = `Computer Score: ${computerScore}`;
-
-    results.appendChild(roundOutcome);
-    results.appendChild(humanScoreText);
-    results.appendChild(computerScoreText);
-}
-
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
+    const menu = document.querySelector("#menu");
+    const results = document.querySelector("#results");
+    
+    const roundOutcome = document.createElement("h3");
+    const humanScoreText = document.createElement("p");
+    const computerScoreText = document.createElement("p");
 
+    menu.addEventListener("click", (event) => {
+        const computerSelection = getComputerChoice();
+        playRound(event.target.id, computerSelection);
+    });
 
-    // for (i = 0; i < 5; i++) {
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();
-    //     playRound(humanSelection, computerSelection);
-    // }
-    // console.log("Game Over!")
-    // if (humanScore == computerScore) {
-    //     console.log(`Score: ${humanScore} vs ${computerScore}... Its a tie!`);
-    // } else if (humanScore > computerScore) {
-    //     console.log(`Score: ${humanScore} vs ${computerScore} You Win!`);
-    // } else {
-    //     console.log(`Score: ${humanScore} vs ${computerScore} You Lose!`);
-    // }
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            roundOutcome.textContent = `${humanChoice} vs ${computerChoice}... Its a tie!`;
+        } else if (
+            (humanChoice == "rock" && computerChoice != "paper") ||
+            (humanChoice == "paper" && computerChoice != "scissors") ||
+            (humanChoice == "scissors" && computerChoice != "rock") ) {
+            
+            humanScore++;
+            roundOutcome.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        } else {
+            computerScore++;
+            roundOutcome.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
+        }
+
+        humanScoreText.textContent = `Player Score: ${humanScore}`;
+        computerScoreText.textContent = `Computer Score: ${computerScore}`;
+
+        if (humanScore >= 5 || computerScore >= 5) {
+            if (humanScore > computerScore) {
+                roundOutcome.textContent = "Game Over! You Win!"
+            } else {
+                roundOutcome.textContent = "Game Over! You Lose!"
+            }
+            humanScore = 0;
+            computerScore = 0;
+        }
+
+        results.appendChild(roundOutcome);
+        results.appendChild(humanScoreText);
+        results.appendChild(computerScoreText);
+    }
 }
